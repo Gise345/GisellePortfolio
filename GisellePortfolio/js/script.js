324f044feae74c8d18fc338a113fce751a8f07ef
Giselle Johnson <165144397+Gise345@users.noreply.github.com>
2024-11-17 00:26:57 +0000

    Delete GisellePortfolio directory

--- GisellePortfolio/js/script.js
+++
@@ -1,9 +0,0 @@
-/** ===== typing Annimation ===== */
-
-var typed = new Typed(".typing", {
-    strings:["", "Full Stack Engineer", "AWS Certified Professional"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop: true
-
-})
